4cc5b1b7f591d8939c7289d8569bf0f75ed87dfb
Anders Einar Hilden <hildenae@gmail.com>
2014-11-05 20:20:00 +0100

    If on a VM, use apt cache

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+# Use apt-acacher if on a virtual machine
+VMXNET=$(cat /proc/modules | grep -q vmxnet; echo $?)
+if [ $VMXNET -eq 0 ]; then
+  echo "On virtual machine, setting up apt proxy"
+  GATEWAY=$(ip route show | grep ^default | cut -d' ' -f 3)
+  HOST="$(echo -n $GATEWAY | cut -d . -f 1-3).$(($(echo $GATEWAY | cut -d . -f 4 )-1))"
+  echo "Acquire::http { Proxy \"http://$HOST:3142\"; };" >> /etc/apt/apt.conf.d/01proxy
+fi
+
 # To prevent warnings about missing locales if we 
 # SSH from a norwegian system 
 
